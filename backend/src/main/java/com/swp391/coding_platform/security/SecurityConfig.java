6ce4e4f6e4730fc8d1f053cc39ff191e4ac15444
Hồ Sĩ Tấn <166429877+Hositan26@users.noreply.github.com>
2026-06-04 16:53:05 +0700

    Update

--- backend/src/main/java/com/swp391/coding_platform/security/SecurityConfig.java
+++ backend/src/main/java/com/swp391/coding_platform/security/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 import java.util.Arrays;
 import java.util.List;
